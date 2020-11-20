cde093335e941631b3a752056a7b4cd59e1f51d6
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-11-20 14:00:06 +0530

    running with session following test, using resources correctly example includes transposition, matrix multiplication, feeding to graphs

--- javaapi/src/HelloTensorFlow.scala
+++ javaapi/src/HelloTensorFlow.scala
@@ -9,15 +9,31 @@ import org.tensorflow.op.core.Placeholder
 import org.tensorflow.op.math.Add
 import org.tensorflow.types.TInt32
 import scala.util.Using
+import org.tensorflow.{Session, Graph, Tensor, _}
+import org.tensorflow.op.linalg.MatMul
+import org.tensorflow.ndarray._
 
 object HelloTensorFlow {
-  def main(args: Array[String]) : Unit = {
+  def main(args: Array[String]): Unit = {
     println("Hello TensorFlow " + TensorFlow.version())
     val x = TInt32.scalarOf(10)
-    Using(ConcreteFunction.create(dblFunc)){dbl =>
-    val dblX = dbl.call(x).expect(TInt32.DTYPE)
-    println(x.data().getInt() + " doubled is " + dblX.data().getInt())
+    Using(ConcreteFunction.create(dblFunc)) { dbl =>
+      val dblX = dbl.call(x).expect(TInt32.DTYPE)
+      println(x.data().getInt() + " doubled is " + dblX.data().getInt())
     }
+    println("Session should output 31")
+    Using.Manager { use =>
+      val g = use(new Graph())
+      val s = use(new Session(g))
+      val tf = Ops.create(g)
+      transpose_A_times_X(tf, Array(Array(2), Array(3)))
+      Using(TInt32.tensorOf(StdArrays.ndCopyOf(Array(Array(5), Array(7))))) {
+        x =>
+          val outputs = s.runner().feed("X", x).fetch("Y").run()
+          println(outputs.get(0).expect(TInt32.DTYPE).data().getInt(0, 0))
+      }
+    }
+
   }
 
   val clTest = implicitly[Using.Releasable[java.lang.AutoCloseable]]
@@ -26,5 +42,15 @@ object HelloTensorFlow {
     val x = tf.placeholder(TInt32.DTYPE);
     val dblX = tf.math.add(x, x);
     Signature.builder().input("x", x).output("dbl", dblX).build();
+  }
+
+  def transpose_A_times_X(tf: Ops, a: Array[Array[Int]]) {
+    tf.withName("Y")
+      .linalg
+      .matMul(
+        tf.withName("A").constant(a),
+        tf.withName("X").placeholder(TInt32.DTYPE),
+        MatMul.transposeA(true).transposeB(false)
+      );
   }
 }
