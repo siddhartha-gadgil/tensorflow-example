3612f8e53356eaa972742f58b384226d2a442b98
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-12-05 11:54:36 +0530

    word2vec runs but gives NaN

--- javaapi/src/Word2Vec.scala
+++ javaapi/src/Word2Vec.scala
@@ -24,13 +24,19 @@ import scala.util.{Try, Random}
 import org.tensorflow.op.linalg.MatMul
 
 object Word2Vec {
+  val windowWidth = 2
+
+  val negSamples = 5
+
+  val embedDim = 20
+
   val tokens: Vector[String] = os.read
     .lines(os.pwd / "javaapi" / "resources" / "shakespeare.txt")
     .flatMap { l =>
-      l.split("\\s+")
+      l.split("\\s+").map(_.toLowerCase())
     }
     .toVector
-    .take(100)
+  // .take(100)
 
   val paddedTokens = "<pad>" +: tokens
 
@@ -41,12 +47,12 @@ object Word2Vec {
   val rnd = new Random()
 
   def randomMatrix(rows: Int, columns: Int): Array[Array[Float]] = {
-    (0 until (rows)).map { i =>
-      (0 until (columns)).map(_ => rnd.nextGaussian().toFloat).toArray
-    }.toArray
+    Array.fill(rows)(Array.fill(columns)(rnd.nextGaussian().toFloat))
   }
 
-  val windowWidth = 2
+  def randomIndex() = rnd.nextInt(vocab.size)
+
+  def negList(n: Int) = (0 until (n)).toList.map(_ => randomIndex())
 
   def getOrderedSkipGrams(tokenVec: Vector[String]): Vector[(String, String)] =
     for {
@@ -61,6 +67,151 @@ object Word2Vec {
 
   val positiveSkipGrams = getSkipGrams(tokens)
 
+  val skipGramIndices = positiveSkipGrams.map { case (word, context) =>
+    (vocab(word), vocab(context))
+  }
+
+  def dataGroup(): Iterator[(Int, Int, List[Int])] = {
+    Iterator.from(skipGramIndices).map { case (word, context) =>
+      (word, context, negList(negSamples))
+    }
+  }
+
+  def trainingData(epochs: Int): Iterator[(Int, Int, List[Int])] =
+    Iterator.range(0, epochs).flatMap(_ => dataGroup())
+
+  def trainedTensors(
+      epochs: Int = 1
+  ): Try[(Tensor[TFloat32], Tensor[TFloat32])] = Using(new Graph) { graph =>
+    val w2v = new Word2Vec(graph, embedDim, vocab.size)
+    val data = trainingData(epochs)
+    println(s"training with data size ${skipGramIndices.size * epochs}")
+    w2v.fit(data)
+  }.flatten
+
+}
+class Word2Vec(
+    graph: Graph,
+    embedDim: Int,
+    vocabSize: Int,
+    learningRate: Float = 0.1f
+) {
+  val tf = Ops.create(graph)
+
+  val wordIndices = tf.withName("word").placeholder(TInt32.DTYPE)
+
+  val wordInputVec = tf.oneHot(
+    wordIndices,
+    tf.constant(vocabSize),
+    tf.constant(1.0f),
+    tf.constant(0.0f)
+  )
+
+  val contextIndices = tf.withName("context").placeholder(TInt32.DTYPE)
+
+  val labels = // tf.constant(Array(0.0f, 1.0f))
+    tf.withName("label").placeholder(TFloat32.DTYPE)
+
+  val contextInputVec = tf.oneHot(
+    contextIndices,
+    tf.constant(vocabSize),
+    tf.constant(1.0f),
+    tf.constant(0.0f)
+  )
+
+  val wordEmbed =
+    tf.variable(tf.constant(Word2Vec.randomMatrix(embedDim, vocabSize)))
+
+  val contextEmbed =
+    tf.variable(tf.constant(Word2Vec.randomMatrix(embedDim, vocabSize)))
+
+  val initialize = tf.withName("init").init()
+
+  val wordVec = {
+    tf
+      .withName("wordvec")
+      .linalg
+      .matMul(
+        wordEmbed,
+        wordInputVec,
+        MatMul.transposeA(false).transposeB(true)
+      )
+  }
+
+  val contextVec = {
+    tf.withName("contextvec")
+      .linalg
+      .matMul(
+        contextEmbed,
+        contextInputVec,
+        MatMul.transposeA(false).transposeB(true)
+      )
+  }
+
+  val dots = tf
+    .withName("dots")
+    .reduceSum(tf.math.mul(wordVec, contextVec), tf.constant(0))
+
+  val predictions = tf.math.sigmoid(dots)
+
+  val cost1 = tf.math.neg(tf.math.mul(labels, tf.math.log(predictions)))
+
+  val cost2 = tf.math.mul(
+    tf.math.sub(tf.constant(1f), labels),
+    tf.math.log(tf.math.sub(tf.constant(1f), predictions))
+  )
+
+  val loss =
+    tf.withName("loss").reduceSum(tf.math.sub(cost1, cost2), tf.constant(0))
+
+  val optimizer = new Adam(graph, learningRate)
+
+  val minimize = optimizer.minimize(loss)
+
+  def fit(
+      data: Iterator[(Int, Int, List[Int])]
+  ): Try[(Tensor[TFloat32], Tensor[TFloat32])] = Using(new Session(graph)) {
+    var count = 0
+    session =>
+      session.run(tf.init())
+      data.foreach { case (word, posContext, negContext) =>
+        if (count % 2000 == 0) println(s"feeding datapoint: $count")
+        count += 1
+        val words =
+          TInt32.tensorOf(
+            StdArrays.ndCopyOf(Array.fill(1 + negContext.size)(word))
+          )
+        val contexts =
+          TInt32.tensorOf(
+            StdArrays.ndCopyOf((posContext :: negContext).toArray)
+          )
+        val labelList =
+          TFloat32.tensorOf(
+            StdArrays.ndCopyOf(
+              (1.0f :: List.fill(negContext.size)(0.0f)).toArray
+            )
+          )
+        session
+          .runner()
+          .feed(wordIndices, words)
+          .feed(contextIndices, contexts)
+          .feed(labels, labelList)
+          .addTarget(minimize)
+          .run()
+      }
+      val output =
+        session.runner().fetch(wordEmbed).fetch(contextEmbed).run()
+      (
+        output.get(0).expect(TFloat32.DTYPE),
+        output.get(1).expect(TFloat32.DTYPE)
+      )
+  }
+
+}
+
+// Below are just tests
+object Word2VecExperiments {
+  import Word2Vec._
   lazy val oneHot = {
     Using(new Graph) { graph =>
       val w2v = new Word2VecExperiments(graph)
@@ -138,77 +289,4 @@ class Word2VecExperiments(graph: Graph) {
       tf.reduceSum(w, tf.constant(Array(0))),
       tf.constant(Array.emptyIntArray)
     )
-}
-
-class Word2Vec(graph: Graph, embedDim: Int, vocabSize: Int) {
-  val tf = Ops.create(graph)
-
-  val wordIndices = tf.withName("word").placeholder(TInt32.DTYPE)
-
-  val wordInputVec = tf.oneHot(
-    wordIndices,
-    tf.constant(vocabSize),
-    tf.constant(1.0f),
-    tf.constant(0.0f)
-  )
-
-  val contextIndices = tf.withName("context").placeholder(TInt32.DTYPE)
-
-  val labels = // tf.constant(Array(0.0f, 1.0f))
-    tf.withName("label").placeholder(TFloat32.DTYPE)
-
-  val contextInputVec = tf.oneHot(
-    contextIndices,
-    tf.constant(vocabSize),
-    tf.constant(1.0f),
-    tf.constant(0.0f)
-  )
-
-  val wordEmbed =
-    tf.variable(tf.constant(Word2Vec.randomMatrix(embedDim, vocabSize)))
-
-  val contextEmbed =
-    tf.variable(tf.constant(Word2Vec.randomMatrix(embedDim, vocabSize)))
-
-  val initialize = tf.withName("init").init()
-
-  val wordVec = {
-    tf
-      .withName("wordvec")
-      .linalg
-      .matMul(
-        wordEmbed,
-        wordInputVec,
-        MatMul.transposeA(false).transposeB(true)
-      )
-  }
-
-  val contextVec = {
-    tf.withName("contextvec")
-      .linalg
-      .matMul(
-        contextEmbed,
-        contextInputVec,
-        MatMul.transposeA(false).transposeB(true)
-      )
-  }
-
-  val dots = tf
-    .withName("dots")
-    .reduceSum(tf.math.mul(wordVec, contextVec), tf.constant(0))
-
-  // val ytrue = tf.withName("ytrue").math.add(labels, tf.constant(Array(0.0f, 0.0f)))
-
-  val predictions = tf.math.sigmoid(dots)
-
-  val cost1 = tf.math.neg(tf.math.mul(labels, tf.math.log(predictions)))
-
-  val cost2 = tf.math.mul(
-    tf.math.sub(tf.onesLike(labels), labels),
-    tf.math.log(tf.math.sub(tf.onesLike(predictions), predictions))
-  )
-
-  val losses =
-     tf.withName("loss").reduceSum(tf.math.sub(cost1, cost2), tf.constant(0))
-
 }
